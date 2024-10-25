e8bf2064da5147d4ee6165fcff26990e0ca54a57
@KaraboMakoro <k.m.makoro@gmail.com>
2024-10-25 11:18:40 +0200

    Added registration and login
    index page added functionality to register and login
    contact us page added send us a message via email

--- functions.js
+++ functions.js
@@ -95,3 +95,23 @@ function loginUser() {
     document.getElementById('loginMessage').textContent = username +  " Successfully logged in!";
 }
 
+//Function to Validate Email
+function validateEmail(email) {
+    var re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return re.test(email);
+}
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || subject === "" || message === "") {
+        alert("All fields are required!");
+        event.preventDefault();
+    } else if (!validateEmail(email)) {
+        alert("Please enter a valid email address!");
+        event.preventDefault();
+    }
+});
+
